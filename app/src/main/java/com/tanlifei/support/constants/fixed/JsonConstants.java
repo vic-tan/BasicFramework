dcceef224da60695f37bb26bbef21d4d3bae03fe
vic-tan <179840045@.com>
2016-08-19 17:13:17 +0800

    框架结构调整

--- app/src/main/java/com/tanlifei/support/constants/fixed/JsonConstants.java
+++ app/src/main/java/com/tanlifei/support/constants/fixed/JsonConstants.java
@@ -9,22 +9,5 @@ package com.tanlifei.support.constants.fixed;
 public class JsonConstants {
 
     // 分页参数
-    public static final String REQUEST_TASK_LIST_PARAM_PAGE_SIZE = "page";//每页数的key值
-    public static final String REQUEST_TASK_LIST_PARAM_PAGE_NUMBER = "pageNumber";//当前页数的key值
     public static final int PAGE_SIZE = 10; // 每页显示 的数据条数
-    public static final int COMMUNITY_MEMBER_PAGE_SIZE = 40; // 每页显示 的数据条数
-
-
-    public static final String JSON_LIST = "mRefreshList";
-    public static final String json_data = "data";
-    public static String JSON_RESULT = "result";//请求接口返回结果
-    public static String JSON_RESULT_SUCCEE = "1";//请求接口返回结果为成功
-    public static String JSON_RESULT_FAILURE = "0";//请求接口返回结果为失败
-    public static final String JSON_TASK_URL = "url";//请求接口url存储map的中的key值
-
-
-
-
-
-
 }
